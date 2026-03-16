50c88434b21f56c1e5adf665c5c31092d1a1c735
typist <1003659191@qq.com>
2026-03-16 19:34:30 +0800

    feat: add main agant

---
+++ agents/main.ts
@@ -0,0 +1,17 @@
+import { openaiProvider, options } from "@/lib/ai"
+import { stepCountIs, ToolLoopAgent } from "ai"
+
+const instructions = "你是一个通用助手，语言言简意赅。"
+
+export const MainAgent = new ToolLoopAgent({
+  ...options,
+  id: "Main Agent",
+  instructions,
+  stopWhen: [stepCountIs(16)],
+  tools: {
+    webSearch: openaiProvider.tools.webSearch({
+      externalWebAccess: true,
+      searchContextSize: 'high',
+    }),
+  },
+})
