f8878e3af023b72cd9c220638804c4659621bb50
typist <1003659191@qq.com>
2026-04-05 22:35:13 +0800

    chore: add ai

---
+++ config.ts
@@ -0,0 +1,12 @@
+import { loadEnvConfig } from '@next/env'
+ 
+const projectDir = process.cwd()
+loadEnvConfig(projectDir)
+
+export const config = {
+  ai: {
+    baseUrl: process.env.AI_BASE_URL?.trim() || "https://api.openai.com/v1",
+    apiKey: process.env.AI_API_KEY?.trim() || "sk-xxx",
+    model: process.env.AI_MODEL?.trim() || "gpt-5.4"
+  }
+}
