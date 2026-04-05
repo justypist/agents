1a9154caffe5f0f5991c8a052c87417ea3a17caa
typist <1003659191@qq.com>
2026-04-06 06:01:29 +0800

    style: pnpm lint

--- lib/ai.ts
+++ lib/ai.ts
@@ -1,10 +1,10 @@
 import { createOpenAI } from '@ai-sdk/openai';
 
-import { config } from '@/config'
+import { config } from '@/config';
 
 export const openai = createOpenAI({
   baseURL: config.ai.baseUrl,
   apiKey: config.ai.apiKey,
 });
 
-export const model = openai.responses(config.ai.model)
+export const model = openai.responses(config.ai.model);
